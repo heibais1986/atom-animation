d8db3bcb1dc7eb60e496d4a33d827bb835732569
matt765 <mateusz.wyrebek@gmail.com>
2025-06-23 03:05:00 +0200

    Install NextJS with TypeScript

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { AtomModel } from "./components/AtomModel/AtomModel";
+import styles from "./page.module.css";
+
+export default function HomePage() {
+  return (
+    <main className={styles.main}>
+      <AtomModel />
+    </main>
+  );
+}
